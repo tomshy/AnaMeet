a59b5bc410e809689e2f320608eef982fc685f21
Tom Nyongesa <tom.nyongesa@zegetech.com>
2020-09-16 00:24:47 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :notes
-  resources :meetings
-  resources :users
+  devise_for :users
+  resources :users do
+    resources :notes
+    resources :meetings
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
